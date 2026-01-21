7280742ca86daa6e831a30f9b0f6219db4e32499
Muhammadqodir779 <kadirovmuhammadqodir663@gmail.com>
2026-01-21 10:10:35 +0500

    Seshanba: Express o‘rnatildi va birinchi GET routelar qo‘shildi

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Salom, Node.js! Server ishlamoqda 🎉');
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Salom, Express! Server ishlamoqda 🚀');
 });
-server.listen(3000, () => {
-  console.log('Server 3000-portda ishlamoqda → http://localhost:3000');
+app.get('/hello', (req, res) => {
+  res.send('Hello World from Express!');
+});
+app.get('/about', (req, res) => {
+  res.send('Bu haqida sahifa. Backend o‘rganayapmiz!');
+});
+app.listen(3000, () => {
+  console.log('Express server 3000-portda ishlamoqda → http://localhost:3000');
 });
