c51a08e2ebf308711fdaaf35d011cba08ebc2d70
Muhammadqodir779 <kadirovmuhammadqodir663@gmail.com>
2026-01-21 11:03:24 +0500

    Chorshanba: POST, PUT, DELETE routelar qo'shildi

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Salom, Express! Server ishlamoqda 🚀');
-});
+
+// Middleware – JSON ma'lumotlarni o‘qish uchun
+app.use(express.json());
+
 app.get('/hello', (req, res) => {
   res.send('Hello World from Express!');
 });
+
 app.get('/about', (req, res) => {
   res.send('Bu haqida sahifa. Backend o‘rganayapmiz!');
 });
+
+app.post('/users', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ message: `Yangi user qo'shildi: ${name}` });
+});
+
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  res.json({ message: `User ${id} yangilandi: ${name}` });
+});
+
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ message: `User ${id} o‘chirildi` });
+});
+
 app.listen(3000, () => {
-  console.log('Express server 3000-portda ishlamoqda → http://localhost:3000');
+  console.log('Server 3000-portda ishlamoqda → http://localhost:3000');
 });
