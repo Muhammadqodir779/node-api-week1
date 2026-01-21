5862870f59eb302892515d2dee6d739a2cb70912
Muhammadqodir779 <kadirovmuhammadqodir663@gmail.com>
2026-01-21 10:03:05 +0500

    node-api-week1

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Salom, Node.js! Server ishlamoqda 🎉');
+});
+server.listen(3000, () => {
+  console.log('Server 3000-portda ishlamoqda → http://localhost:3000');
+});
